3b57aac0fa1bd05496599ccb78f5386e00fe703a
oxi <abdoumjr@gmail.com>
2022-08-16 01:05:27 +0100

    update urls for testing

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adc213e1e3ace4767937de41e4219394-908070777.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
